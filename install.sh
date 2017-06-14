4ad4b9b30a6f586796f12c9af799bac2acf22ed1
Stefan Imhoff <stefan@imhoff.name>
2017-06-14 18:52:23 +0200

    Add base16-shell activation to install

--- install.sh
+++ install.sh
@@ -49,5 +49,10 @@ source install/vim-plugins.sh
 # Symlink Neovim
 source install/neovim.sh
 
+# Activate Base16-Shell
+BASE16_SHELL=$HOME/.config/base16-shell/
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+base16_tomorrow-night
+
 # Activate zsh
 chsh -s /bin/zsh
