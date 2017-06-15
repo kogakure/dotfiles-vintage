122246f8220055d1df5b56986730121287ca8b7d
Stefan Imhoff <stefan@imhoff.name>
2017-06-15 13:19:55 +0200

    Update cask file

--- install/cask.sh
+++ install/cask.sh
@@ -7,21 +7,27 @@
 brew tap caskroom/cask
 brew tap caskroom/versions
 
+# XQuartz needs to be installed first
+brew cask install xquartz
+
 #############
 ### Casks ###
 #############
 
-brew cask install 1password
+# brew cask install 1password # Install from Store for TouchID
 brew cask install adobe-design-web-premium-cs6-de
 brew cask install alfred
 brew cask install bartender
 brew cask install blender
 brew cask install brave
 brew cask install calibre
+brew cask install ccmenu
 brew cask install color-oracle
+brew cask install cryptomator
 brew cask install daisydisk
 brew cask install dash
 brew cask install delicious-library
+brew cask install devonthink-pro-office
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
@@ -31,21 +37,21 @@ brew cask install fitbit-connect
 brew cask install flip4mac
 brew cask install fontexplorer-x-pro
 brew cask install fontforge
-brew cask install gemini
+# brew cask install gemini # Using version 1.5 from AppStore
+brew cask install gitbook-editor
 brew cask install github-desktop
-brew cask install gitxinstall
 brew cask install google-chrome
 brew cask install google-chrome-beta
 brew cask install google-chrome-canary
 brew cask install google-chrome-dev
 brew cask install google-drive
-brew cask install google-earth
+brew cask install google-earth-pro
 brew cask install google-earth-web-plugin
 brew cask install google-hangouts
 brew cask install google-japanese-ime
 brew cask install google-photos-backup
 brew cask install gpgtools
-brew cask install growlnotify
+# brew cask install growlnotify
 brew cask install hammerspoon
 brew cask install handbrake
 brew cask install hipchat
@@ -54,11 +60,11 @@ brew cask install imageoptim
 brew cask install integrity
 brew cask install iterm2
 brew cask install kaleidoscope
+brew cask install kindle
 brew cask install lastfm
 brew cask install livereload
-brew cask install logitech-control-center
 brew cask install macvim
-brew cask install makemkv
+# brew cask install makemkv
 brew cask install marked
 brew cask install mousepose
 brew cask install mysqlworkbench
@@ -67,18 +73,16 @@ brew cask install opera
 brew cask install opera-beta
 brew cask install opera-developer
 brew cask install origin
+brew cask install owncloud
 brew cask install p4merge
+brew cask install postman
 brew cask install qlvideo
+brew cask install riot
 brew cask install robomongo
-brew cask install seil
 brew cask install sequel-pro
 brew cask install sitesucker
-brew cask install sketchup
-brew cask install skitch
-brew cask install skyfonts
+brew cask install skyfonts # Run installer later
 brew cask install skype
-brew cask install slack
-brew cask install sonos
 brew cask install spotify
 brew cask install spotify-notifications
 brew cask install sqlitebrowser
@@ -98,4 +102,5 @@ brew cask install wacom-bamboo-tablet
 brew cask install wd-my-cloud
 brew cask install webkit-nightly
 brew cask install whatroute
-brew cask install wordpresscom
+brew cask install whatsapp
+brew cask install xmind
