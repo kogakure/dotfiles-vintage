1589bb0d69d436f563d19b5365ff374c9d0bea57
Stefan Imhoff <stefan@imhoff.name>
2018-09-29 10:17:54 +0200

    Update Mail settings for Hammerspoon

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -70,7 +70,7 @@ local LAYOUT_DUAL = {
   {"Google Chrome",         nil, DISPLAY_MAIN,     FULLSCREEN, nil, nil},
   {"Google Hangouts",       nil, DISPLAY_MAIN,     LEFT_HALF,  nil, nil},
   {"Kalender",              nil, DISPLAY_MAIN,     LEFT_HALF,  nil, nil},
-  {"Mail",                  nil, DISPLAY_MAIN,     LEFT_HALF,  nil, nil},
+  {"Mail",                  nil, DISPLAY_MAIN,     LEFT_MOST,  nil, nil},
   {"Safari",                nil, DISPLAY_NOTEBOOK, FULLSCREEN, nil, nil},
   {"Sonos",                 nil, DISPLAY_MAIN,     FULLSCREEN, nil, nil},
   {"Spotify",               nil, DISPLAY_NOTEBOOK, FULLSCREEN, nil, nil},
